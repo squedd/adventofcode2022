f733409db74afd037fd35d01502e0335afec1d9c
squedd <40431572+squedd@users.noreply.github.com>
2022-12-03 02:49:13 +1000

    Day 2

---
+++ day2.py
@@ -0,0 +1,46 @@
+FILE = "day2.txt"
+
+def process_file(file):
+    with open(file) as f:
+        output = [game.split() for game in f.read().split('\n')]
+        return output
+
+def part1(file):
+    WIN, DRAW, LOSS = 6, 3, 0
+    P1_CODES, P2_CODES = ['A', 'B', 'C'], ['X', 'Y', 'Z']
+    P1_WIN_CONDITIONS = ['Z', 'X', 'Y']
+    P2_WIN_CONDITIONS = ['C', 'A', 'B']
+
+    games = process_file(file)
+    points = 0
+    for p1, p2 in games:
+        if p1 in P1_CODES and p2 in P2_CODES:
+            if P2_WIN_CONDITIONS[P2_CODES.index(p2)] == p1:
+                points += WIN
+            elif P1_WIN_CONDITIONS[P1_CODES.index(p1)] == p2:
+                points += LOSS
+            else:
+                points += DRAW
+            points += P2_CODES.index(p2) + 1
+
+    return points
+
+def part2(file):
+    CODES = ['A', 'B', 'C']
+    CONDITION_CODES = ['X', 'Y', 'Z']
+    CONDITIONS = {'A': ['C', 'A', 'B'], 'B': ['A', 'B', 'C'], 'C': ['B', 'C', 'A']}
+
+    games = process_file(file)
+    points = 0
+    for p1_shape, condition in games:
+        conditions = CONDITIONS[p1_shape]
+        condition_index = CONDITION_CODES.index(condition)
+        chosen_shape = conditions[condition_index]
+        points += CODES.index(chosen_shape) + 1
+        points += (condition_index * 3)
+
+    return points
+
+
+print("Part 1:", part1(FILE))
+print("Part 2:", part2(FILE))
