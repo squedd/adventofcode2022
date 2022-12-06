5f61b8411c4a56670033eeff8ebb518d9a441bcd
squedd <40431572+squedd@users.noreply.github.com>
2022-12-07 02:49:51 +1000

    Day 6

---
+++ day6.py
@@ -0,0 +1,24 @@
+def process_file(file="day6.txt"):
+    with open(file) as f:
+        data_stream = f.read().strip()
+        return data_stream
+
+def find_start_of_packet(distinct_chars):
+    data_stream = process_file()
+    for first_marker, char in enumerate(range(len(data_stream)-distinct_chars)):
+        message_to_check = data_stream[first_marker:first_marker+distinct_chars]
+        if len(set(message_to_check)) == distinct_chars:
+            return first_marker + distinct_chars
+    return -1
+
+def part1():
+    return find_start_of_packet(4)
+
+def part2():
+    return find_start_of_packet(14)
+
+def solution():
+    print("Part 1:", part1())
+    print("Part 2:", part2())
+
+solution()
